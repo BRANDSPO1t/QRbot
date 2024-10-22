0ff58cb247aaeb5bf881713b511ff46ceb0df4bf
BRANDSPO1t <162730938+BRANDSPO1t@users.noreply.github.com>
2024-10-22 20:11:58 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+import qrcode
+import telebot
+
+bot = telebot.TeleBot('7191362631:AAHo-2VbPgmJdBjsMwEDQhoKLArjt7qkRf0')
+
+@bot.message_handler(commands = ['start'])
+def main(message):
+    bot.send_message(message.chat.id, f'Здравствуйте, {message.from_user.last_name} {message.from_user.first_name}! Этот бот конвертирует информацию в QRcode!')
+    bot.send_message(message.chat.id, 'Напиши текст, кторый хочешь конвертировать в QRcode')
+    bot.register_next_step_handler(message, qr)
+
+def qr(message):
+    data = message.text
+    img = qrcode.make(data)
+    type(img)
+    img.save('qrfile.png')
+    fileop = open('qrfile.png', 'rb')
+    bot.send_photo(message.chat.id, fileop)
+
+bot.polling(none_stop = True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
